a7bcc6fb9bd80e3631fffa5e9d0835f12d837711
si-saaref <achmadsyarif241@gmail.com>
2024-06-10 05:52:42 +0700

    feat: create api for get one film

--- app/api/films/controller.js
+++ app/api/films/controller.js
@@ -1,12 +1,34 @@
 const { Film } = require('../../db/models');
 const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
-const { passwordChecker, usernameChecker, emailChecker } = require('../../helper/helper');
 
 module.exports = {
 	getAllFilms: async (req, res) => {
 		try {
 			const filmPayload = await Film.findAll();
+			res
+				.status(200)
+				.json({ message: 'Successfully Get All Films Data', status: 200, data: filmPayload });
+		} catch (error) {
+			res.status(500).json({ message: error.message || 'Internal Message Error', status: 500 });
+		}
+	},
+	getOneFilm: async (req, res) => {
+		try {
+			const { idFilm } = req.params;
+			const filmPayload = await Film.findOne({
+				where: {
+					id: idFilm,
+				},
+			});
+			if (!filmPayload) {
+				res.status(404).json({
+					message: 'Film you are looking for is not exist. Please try another one.',
+					status: 404,
+					data: {},
+				});
+				return;
+			}
 			res
 				.status(200)
 				.json({ message: 'Successfully Get All Films Data', status: 200, data: filmPayload });
--- app/api/films/router.js
+++ app/api/films/router.js
@@ -1,8 +1,9 @@
 const express = require('express');
-const { getAllFilms } = require('./controller');
+const { getAllFilms, getOneFilm } = require('./controller');
 const router = express.Router();
 
 /* GET home page. */
 router.get('/', getAllFilms);
+router.get('/:idFilm', getOneFilm);
 
 module.exports = router;
